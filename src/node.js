c0ad4bd24f7a524b7f7afa8ef1d5c325ad5deb70
Irvin Zhan <izhan@princeton.edu>
2015-12-28 17:15:58 -0500

    passing tests for node addchild

--- src/node.js
+++ src/node.js
@@ -1,9 +1,22 @@
 (function () {
   Node = function(name, collapsed) {
-    
+    this.name = name;
+    this.collapsed = collapsed;
+    this.children = [];
+    this.path = "/";
   };
 
   Node.prototype.addChild = function(name, collapsed) {
+    var childNode = new Node(name, collapsed);
+    this.children.push(childNode);
+    childNode.parent = this;
 
+    if (this.path == "/") {
+      childNode.path = this.path + name;
+    } else {
+      childNode.path = this.path + "/" + name;
+    }
+    
+    return childNode;
   };
 }());
