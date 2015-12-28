b0e2be51b9e745f3d5714f191fcaabd0b082a0e7
Irvin Zhan <izhan@princeton.edu>
2015-12-28 17:02:06 -0500

    adding jshint grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,25 @@ module.exports = function(grunt){
       options: {
         run: true
       }
+    },
+
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
     }
   });
 
-  // Load grunt mocha task
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['mocha']);
+  grunt.registerTask('default', ['mocha', 'jshint']);
 };
