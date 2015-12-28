f40475af722af2b0e518297f888c62ccbf4f7250
Irvin Zhan <izhan@princeton.edu>
2015-12-28 17:29:38 -0500

    adding tree specs and tree constructor

--- test/tree_spec.js
+++ test/tree_spec.js
@@ -1,6 +1,11 @@
 // tests.js
 describe('Tree', function(){
-  it('should do something', function(){
-    assert.equal(1, 1);
+  describe('constructor', function(){
+    var rootNode = new Node("foo", false);
+    var tree = new Tree(rootNode);
+
+    it('should simply keep track of the root node', function(){
+      expect(tree.rootNode).to.be.equal(rootNode);
+    });
   });
 });
