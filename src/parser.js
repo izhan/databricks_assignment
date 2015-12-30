3cc2844f51788a2bc587df64f0920045d1667ba9
Irvin Zhan <izhan@princeton.edu>
2015-12-29 19:49:23 -0500

    fixing parser path bug

--- src/parser.js
+++ src/parser.js
@@ -1,22 +1,28 @@
 var Parser = (function () {
   /**
+   * Recursively converts the entire data into a tree rooted at the return node
    * @param  {Object} data The JSON representation of the node.
    * @return {BrowserNode} The BrowserNode object represented by the JSON
    * @private
    */
-  var convertToNode = function(data) {
-    var currentNode = new BrowserNode(data.name, data.collapsed);
+  var convertToRootNode = function(inputData) {
+    var rootNode = new BrowserNode(inputData.name, inputData.collapsed);
 
-    if (!data.children) {
-      return currentNode;
+    // needs to actually be BFS to have proper path inheritance
+    var queue = [[rootNode, inputData]];
+
+    while (queue.length != 0) {
+      var next = queue.shift();
+      var node = next[0];
+      var data = next[1];
+      for (var i = 0; data.children && i < data.children.length; i++) {
+        var child = new BrowserNode(data.children[i].name, data.children[i].collapsed);
+        node.appendChild(child);
+        queue.push([child, data.children[i]]);
+      }
     }
 
-    for(var i = 0; i < data.children.length; i++) {
-      var child = convertToNode(data.children[i]);
-      currentNode.appendChild(child);
-    }
-
-    return currentNode;
+    return rootNode;
   };
 
   return {
@@ -25,7 +31,7 @@ var Parser = (function () {
      * @return {BrowserTree} The BrowserTree object represented by the JSON
      */
     initTree: function(data) {
-      var rootNode = convertToNode(data);
+      var rootNode = convertToRootNode(data);
       return new BrowserTree(rootNode);
     }
   };
